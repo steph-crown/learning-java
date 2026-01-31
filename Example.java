1e8c37685a544bc5a0e43fc31bd5d869cdf48d51
steph-crown <emmanuelstephen024@gmail.com>
2026-01-31 22:40:28 +0100

    Update

--- Example.java
+++ Example.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Give the first number:");
+//        int first = Integer.parseInt(scanner.nextLine());
+//        System.out.println("Give the second number:");
+//        int second = Integer.parseInt(scanner.next());
+//
+//        System.out.println(first + " + " + second + " = " + (first + second));
+//
 
-        System.out.println("Fuck yeah, Text to be printed");
-        String message = scanner.nextLine();
-        boolean bool = Boolean.parseBoolean(message);
-        System.out.println("message: " + (bool && false));
+//        arithmetic
+        double x = (double) 4;
+        System.out.println("double " + x);
 
-        int s = 3;
-        double kl = s;
-        int j = s;
+        int y = (int) 3.8;
+        System.out.println("y " + y);
 
-
+        int z = y;
+        System.out.println("y " + y);
     }
 }
