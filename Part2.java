3c93134d3414614a1f460f1e1600cc6c093069dd
steph-crown <emmanuelstephen024@gmail.com>
2026-02-02 17:20:54 +0100

    Update

--- Part2.java
+++ Part2.java
@@ -1,5 +1,53 @@
 void main() {
-    readAndAddNumbers();
+//    readAndAddNumbers();
+//    printAndLoop();
+    countOnes();
+}
+
+public static void countOnes() {
+    /*
+        Let's create a program to count and print out the number of ones entered by the user. Let's
+        first create a non-working version and examine the action of the blocks.
+     */
+    Scanner scanner = new Scanner(System.in);
+    int onesCount = 0;
+
+    while (true) {
+        System.out.println("Enter new number (0 exits)");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if (number == 1) {
+            onesCount += 1;
+        } else if (number == 0) {
+            break;
+        }
+
+        System.out.println("The number of ones is " + onesCount);
+    }
+}
+
+public static void printAndLoop() {
+/*
+        Write a program that asks the user for numbers. If the number is negative (smaller than
+        zero), the program prints for user "Unsuitable number" and asks the user for a new number.
+        If the number is zero, the program exits the loop. If the number is positive, the program
+        prints the number to the power of two.
+ */
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+        System.out.println("Enter a new number");
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if (num == 0) {
+            break;
+        } else if (num > 0) {
+            System.out.println(Math.pow(num, 2));
+        } else {
+            System.out.println("Unsuitable number");
+        }
+    }
+
 }
 
 
