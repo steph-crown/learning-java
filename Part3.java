2c1aadb43f30f7a83dd75872c3ea46d4ce895c9a
steph-crown <emmanuelstephen024@gmail.com>
2026-02-04 15:55:52 +0100

    Update

---
+++ Part3.java
@@ -0,0 +1,15 @@
+void main() {
+    ArrayList<Integer> arr = new ArrayList<>(List.of(2, 3, 4, 5));
+    arr.add(90);
+    System.out.println(arr.size());
+
+    int[] dance = {2, 3, 4};
+    dance[0] = 1;
+    dance[2] = 9;
+//    System.out.println(dance.c);
+
+    String fuck = "fuck";
+
+    System.out.println(dance.length);
+//    System.out.println("*".repeat(8));
+}
