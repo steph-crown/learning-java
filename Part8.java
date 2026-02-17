ecac72fe54b92b9846d209cb1bf8cb29b14a664f
steph-crown <emmanuelstephen024@gmail.com>
2026-02-17 17:13:59 +0100

    Update

--- Part8.java
+++ Part8.java
@@ -1,46 +1,46 @@
 void main() {
-//    Scanner scanner = new Scanner(System.in);
-//    Container first = new Container("first", 100);
-//    Container second = new Container("second", 100);
-//
-//    while (true) {
-//        String input = scanner.nextLine();
-//
-//        if (input.equals("quit")) {
-//            break;
-//        }
-//
-//        String[] tokens = input.split(" ");
-//        String command = tokens[0];
-//
-//        int value;
-//        try {
-//            value = Integer.parseInt(tokens[1]);
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            System.out.println("Invalid input, try again");
-//            continue;
-//        }
-//
-//        switch (command) {
-//            case "add":
-//                first.add(value);
-//                break;
-//            case "move":
-//                int valueToMove = Math.min(value, first.contains());
-//                first.remove(valueToMove);
-//                second.add(valueToMove);
-//                break;
-//            case "remove":
-//                second.remove(value);
-//                break;
-//            case null, default:
-//                System.out.println("Invalid input, try again");
-//        }
-//
-//        System.out.println(first);
-//        System.out.println(second);
-//    }
+    Scanner scanner = new Scanner(System.in);
+    Container first = new Container("first", 100);
+    Container second = new Container("second", 100);
+
+    while (true) {
+        String input = scanner.nextLine();
+
+        if (input.equals("quit")) {
+            break;
+        }
+
+        String[] tokens = input.split(" ");
+        String command = tokens[0];
+
+        int value;
+        try {
+            value = Integer.parseInt(tokens[1]);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Invalid input, try again");
+            continue;
+        }
+
+        switch (command) {
+            case "add":
+                first.add(value);
+                break;
+            case "move":
+                int valueToMove = Math.min(value, first.contains());
+                first.remove(valueToMove);
+                second.add(valueToMove);
+                break;
+            case "remove":
+                second.remove(value);
+                break;
+            case null, default:
+                System.out.println("Invalid input, try again");
+        }
+
+        System.out.println(first);
+        System.out.println(second);
+    }
 
     HashMap<String, String> map = new HashMap<>();
     map.put("jsjs", "sksk");
